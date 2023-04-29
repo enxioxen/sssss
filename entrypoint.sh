40836d1678f8cc4205ee07e1c99ff4f053fe87d6
enxioxen <118874485+enxioxen@users.noreply.github.com>
2023-04-29 09:23:24 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ UUID=${UUID:-'20f51238-fd38-4b7f-a80c-0a5bed39d284'}
 PAAS1_URL=https://well-hushed-jackrabbit.glitch.me/
 PAAS2_URL=https://superficial-excited-carpet.glitch.me/
 PAAS3_URL=https://tasty-selective-bowl.glitch.me/
-PAAS4_URL=
+PAAS4_URL=https://sssss-4ywn.onrender.com/
 PAAS5_URL=
 PAAS6_URL=
 
